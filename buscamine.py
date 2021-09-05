c0f2c5f6a9db42830b49dd360f8b78f3cc7aac78
DanielMilstein <dnmilstein@miuandes.cl>
2021-09-05 19:37:32 -0300

    Commit antes de cambiar todo drasticamente

--- buscamine.py
+++ buscamine.py
@@ -23,105 +23,98 @@ def gen_minas(tabl, k):
 			tabl[y][x] = 'M'
 	return minas
 
+def verif(x):
+	if x != 'M':
+		str(int(x)+1)
+		return
+	else:
+		return
+
 
 def algoritmo_minas(tab, minas):
 	n = len(tab[0])
 	for (x, y) in minas:
 		#Casos limite, ie: bordes y esquinas
 		#Esquinas (Primero para no repetir condiciones)
-		if x == 0 and y == 0:
-			try:
-				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-			except:
-				continue
+		if x == 0 and y == 0:	
+			tab[y+1][x+1] = str(int(tab[y+1][x+1])+1) if tab[y+1][x+1] != 'M' else tab[y+1][x+1] = tab[y+1][x+1]
+			tab[y][x+1] = str(int(tab[y][x+1])+1) if tab[y][x+1] != 'M' else tab[y][x+1] = tab[y][x+1]
+			tab[y+1][x] = str(int(tab[y+1][x])+1) if tab[y+1][x] != 'M' else tab[y+1][x] = tab[y+1][x]
+
 	
 		elif x == n-1 and y == n-1:
 			try:
-				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1)
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y-1][x] = str(int(tab[y-1][x])+1)
+				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1) if tab[y-1][x-1] != 'M' else tab[y-1][x-1] = tab[y-1][x-1]
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if tab[y][x-1] != 'M' else tab[y][x-1] = tab[y][x-1]
+				tab[y-1][x] = str(int(tab[y-1][x])+1) if tab[y-1][x] != 'M' else tab[y-1][x] = tab[y-1][x]
 			except:
 				continue
 
 		elif x == 0 and y == n-1:
 			try:
-				tab[y-1][x] = str(int(tab[y-1][x])+1)
-				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
+				tab[y-1][x] = str(int(tab[y-1][x])+1) if __ != 'M' else __ = __
+				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1) if __ != 'M' else __ = __
+				tab[y][x+1] = str(int(tab[y][x+1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		elif x == n-1 and y == 0:
 			try:
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		#Bordes
 		elif x == 0:
 			try:
-				tab[y-1][x] = str(int(tab[y-1][x])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1)
-				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1)
+				tab[y-1][x] = str(int(tab[y-1][x])+1) if __ != 'M' else __ = __
+				tab[y][x+1] = str(int(tab[y][x+1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
+				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1) if __ != 'M' else __ = __
+				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		elif y == 0:
 			try:
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
-				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1)
-				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1)			
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
+				tab[y][x+1] = str(int(tab[y][x+1])+1) if __ != 'M' else __ = __
+				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		elif x == n-1:
 			try:
-				tab[y-1][x] = str(int(tab[y-1][x])+1)
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1)
-				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1)
+				tab[y-1][x] = str(int(tab[y-1][x])+1) if __ != 'M' else __ = __
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
+				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		elif y == n-1:
 			try:
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
-				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1)
-				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1)
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
+				tab[y][x+1] = str(int(tab[y][x+1])+1) if __ != 'M' else __ = __
+				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1) if __ != 'M' else __ = __
+				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 		#Else...
 		else:
 			try:
-				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1)
-				tab[y-1][x] = str(int(tab[y-1][x])+1)
-				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1)
-				tab[y][x-1] = str(int(tab[y][x-1])+1)
-				tab[y][x+1] = str(int(tab[y][x+1])+1)
-				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1)
-				tab[y+1][x] = str(int(tab[y+1][x])+1)
-				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1)
+				tab[y-1][x-1] = str(int(tab[y-1][x-1])+1) if __ != 'M' else __ = __
+				tab[y-1][x] = str(int(tab[y-1][x])+1) if __ != 'M' else __ = __
+				tab[y-1][x+1] = str(int(tab[y-1][x+1])+1) if __ != 'M' else __ = __
+				tab[y][x-1] = str(int(tab[y][x-1])+1) if __ != 'M' else __ = __
+				tab[y][x+1] = str(int(tab[y][x+1])+1) if __ != 'M' else __ = __
+				tab[y+1][x-1] = str(int(tab[y+1][x-1])+1) if __ != 'M' else __ = __
+				tab[y+1][x] = str(int(tab[y+1][x])+1) if __ != 'M' else __ = __
+				tab[y+1][x+1] = str(int(tab[y+1][x+1])+1) if __ != 'M' else __ = __
 			except:
 				continue
 
-
-
-def mini_alg(tab, x, y): # Intentar excluir terminos mas que agregar
-	if x != 0:
-		tab[y-1][x-1] = str(int(tab[y-1][x-1])+1)
-	tab[y-1][x] = str(int(tab[y-1][x])+1)
-	tab[y-1][x+1] = str(int(tab[y-1][x+1])+1)
-	tab[y][x-1] = str(int(tab[y][x-1])+1)
-	tab[y][x+1] = str(int(tab[y][x+1])+1)
-	tab[y+1][x-1] = str(int(tab[y+1][x-1])+1)
-	tab[y+1][x] = str(int(tab[y+1][x])+1)
-	tab[y+1][x+1] = str(int(tab[y+1][x+1])+1)
 
 
 
